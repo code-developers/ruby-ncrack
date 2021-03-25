473b3961f15a2fbcd082a11c222be9bc612e3b7c
krishpranav <krisna.pranav@gmail.com>
2021-03-25 15:42:39 +0530

    done changes

--- lib/ncrack/program.rb
+++ lib/ncrack/program.rb
@@ -1,1 +1,19 @@
 require 'ncrack/task'
+
+require 'program/program'
+
+module Ncrack
+  class Program < RProgram::Program
+
+    name_program 'ncrack'
+
+    def self.crack(options={},exec_options={},&block)
+      final.crack(options,exec_options,&block)
+    end
+
+    def crack(options={},exec_options={},&block)
+      run_task(Task.new(options,&block),exec_options)
+    end
+
+  end
+end
